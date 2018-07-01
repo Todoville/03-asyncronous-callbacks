446f7a8d9b35f0ebd8a14fab988fc22314cfba5a
todoville <todoville@gmail.com>
2018-07-01 16:18:36 -0700

    tests passing, linter passing

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
-const fileReader = require('./lib/file-reader')
+const fileReader = require('./lib/file-reader');
 const logger = require('./lib/logger');
 
 const text1 = `${__dirname}/./lib/assets/1.txt`;
 const text2 = `${__dirname}/./lib/assets/2.txt`;
 const text3 = `${__dirname}/./lib/assets/3.txt`;
 
-console.log(fileReader.readThreeFiles(text1, text2, text3, (err, data1, data2, data3) => {
+console.log(fileReader.readThreeFiles(text1, text2, text3, (err, data1, data2, data3) => { /* eslint-disable-line */
   logger.log(logger.INFO, data1);
   logger.log(logger.INFO, data2);
   logger.log(logger.INFO, data3);
 }));
-
-console.log('howdy');
